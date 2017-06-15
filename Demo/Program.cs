9ab885b3e52943f2df5295d4d706d160dc865255
sunzhenyong <sunzhenyong@beisen.com>
2017-06-15 11:22:03 +0800

    Html转换

---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Demo
+{
+    class Program
+    {
+        public static Dictionary<string, object> Properties = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+        static void Main(string[] args)
+        {
+         
+
+        }
+    
+    }
+
+}
---
+++ HtmlConvert/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HtmlConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = @"&lt;p&gt;替换前，，&lt;/p&gt;&lt;p&gt;hello，，，&lt;/p&gt;&lt;p&gt;替换前，，，，&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;";
+            #region 方案一
+            var convertHtmlOne = NoHTML(str);
+            var convertValueOne = ReplaceHtmlTag(convertHtmlOne);
+            #endregion
+            #region 方案二
+            var convertHtmlTwo = System.Web.HttpUtility.HtmlDecode(str);
+            var convertValueTwo = ReplaceHtmlTag(convertHtmlTwo);
+            #endregion
+        }
+        public static string ReplaceHtmlTag(string html, int length = 0)
+        {
+            string strText = System.Text.RegularExpressions.Regex.Replace(html, "<[^>]+>", "");
+            strText = System.Text.RegularExpressions.Regex.Replace(strText, "&[^;]+;", "");
+
+            if (length > 0 && strText.Length > length)
+                return strText.Substring(0, length);
+
+            return strText;
+        }
+        public static string NoHTML(string Htmlstring)
+        {
+
+            //删除脚本  &amp;sect;  
+
+            Htmlstring = Htmlstring.Replace("&sect;", "");
+            Htmlstring = Htmlstring.Replace("&mdash;", "-");
+            Htmlstring = Htmlstring.Replace("&middot;", ".");
+
+            Htmlstring = Regex.Replace(Htmlstring, @"<script[^>]*?>.*?</script>", "", RegexOptions.IgnoreCase);
+
+            //删除HTML    
+
+            Htmlstring = Regex.Replace(Htmlstring, @"<(.[^>]*)>", "", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"([/r/n])[/s]+", "", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"-->", "", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"<!--.*", "", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(quot|#34);", "/", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(amp|#38);", "&", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(lt|#60);", "<", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(gt|#62);", ">", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(nbsp|#160);", " ", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(iexcl|#161);", "/xa1", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(cent|#162);", "/xa2", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(pound|#163);", "/xa3", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&(copy|#169);", "/xa9", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&#(/d+);", "", RegexOptions.IgnoreCase);
+
+
+            Htmlstring.Replace("<", "");
+            Htmlstring.Replace(">", "");
+
+
+
+            Htmlstring.Replace("/r/n", "");
+
+
+            return Htmlstring;
+
+        }
+    }
+}
